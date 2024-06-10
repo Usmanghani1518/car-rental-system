42f5d6923d92c5d6450db35b8284680e3104133e
Ghani <bsf2105191@gmail.com>
2024-06-10 11:20:30 +0500

    fix 2

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from "react";
-import { InputFieldProp } from "@/Types/ui";
+import { InputFieldProp } from "@/Types/ui.ts";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 export const Input: FC<InputFieldProp> = (prop) => {
