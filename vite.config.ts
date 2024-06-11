ebec5916ea29d220dbae7394da3d8775276eb78c
usman <bsf2105191@gmail.com>
2024-06-11 07:52:51 +0500

    feat:deploy issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   resolve: { alias: { "@": "/src" } },
-  base: "/car-rent/",
-  build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: './index.html', // Ensure this path is correct
-    },
-  },
-
 })
