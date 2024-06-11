6656320fb48ffb6ef9d5b3dca7e9fb1149917a3f
usman <bsf2105191@gmail.com>
2024-06-11 07:48:26 +0500

    feat:deploy issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 const App = () => {
 	return (
 		<>
-			{/* <div className="text-2xl text-red-600">header</div> */}
+			<div className="text-2xl text-red-600">header</div>
 			<Outlet />
-			{/* <div className="text-2xl text-red-600">footer</div> */}
+			<div className="text-2xl text-red-600">footer</div>
 		</>
 	);
 };
